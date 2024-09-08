1e102aab227cbd9a132f5ad2b49961b611e1a581
SE Software & Web Developers <hamduleshamsuddin@gmail.com>
2024-09-08 22:13:09 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -54,4 +54,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-        
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
